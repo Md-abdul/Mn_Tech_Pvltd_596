11c1a1a457d7f76d5ccff843fcd6b64aa34e1a63
Md-abdul <mdabdulq62@gmail.com>
2025-06-16 22:06:47 +0530

    new one

--- server/utils/googleClient.js
+++ server/utils/googleClient.js
@@ -3,7 +3,7 @@ const { google } = require("googleapis");
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3001/auth/google/callback" // Hardcode the callback URL for development
+  "https://mn-tech-pvltd-596-1.onrender.com/auth/google/callback" // Hardcode the callback URL for development
 );
 
 module.exports = { oauth2Client };
